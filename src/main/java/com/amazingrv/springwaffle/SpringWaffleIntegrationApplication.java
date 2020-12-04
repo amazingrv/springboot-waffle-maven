0d7e82f3873fd145c3df0740605c37a6324e425f
Rajat Verma <i.rajatverma21@gmail.com>
2020-12-04 14:41:08 +0530

    chore: Initial Commit

---
+++ src/main/java/com/amazingrv/springwaffle/SpringWaffleIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.amazingrv.springwaffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringWaffleIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWaffleIntegrationApplication.class, args);
+    }
+
+}
